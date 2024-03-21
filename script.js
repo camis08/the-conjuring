80bc36ded7aa187cc487911a02d7482963b4ea63
camis08 <alejandracamila770@gmail.com>
2024-03-21 08:58:25 -0300

    aula java

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log("hdbsk");
+/*
+let variavel1 = 0;
+variavel1 = "todo mundo em pânico";
+
+const PI = 3.14;
+
+console.log(typeof(variavel1));
+
+PI = 3.15;
+*/
+let var1 = "30";
+let var2 = 5;
+
+let resultado1 = var1 - var2;
+let resultado2 = var1 + var2;
+
+console.log(var1 * var2);
+console.log(var1 / var2);
+
+console.log(resultado1); 
+console.log(typeof(resultado1)); 
+
+console.log(resultado2); 
+console.log(typeof(resultado2)); 
