de2541adbac9aa41f1520a851ac61f1d8d94e96c
Camila <162577864+camis08@users.noreply.github.com>
2024-04-25 10:47:26 -0300

    Add files via upload
    25/04 PT.2

---
+++ conversor de moedas/conversorMoedas.js
@@ -0,0 +1,165 @@
+let valoresConversao = {
+    real: {
+        dolar: 0.27,
+        euro: 0.18
+    },
+    dolar: {
+        real: 5.03,
+        euro: 0.92,
+    },
+    euro: {
+        real: 5.47,
+        dolar: 0.92
+    }
+}
+
+
+
+let botaoConverter= document.getElementById("botao-converter");
+botaoConverter.addEventListener("click", converter);
+
+let botaoLimpar= document.getElementById("botao-limpar");
+botaoLimpar.addEventListener("click", limpar);
+
+
+let botaoInverter = document.getElementById("botao-inverter");
+botaoInverter.addEventListener("click", inverter);
+
+let botaoAceitaMensagem = document.getElementById("botao-aceita-mensagem");
+botaoAceitaMensagem.addEventListener("click", aceitaMensagem);
+
+if(localStorage.getItem("aceitouCookie" == "1")){
+    aceitaMensagem();
+
+}
+
+function buscaAPI() {
+    let url = "https://economia.awesomeapi.com.br/json/last/USD-BRL";
+    fetch(url).then(function(data){
+        if(data.status == 200) {
+            console.log("retorno ok!");
+        }
+        console.log(data);
+    }).catch();
+}
+
+function converter () {
+    buscaAPI;
+}
+
+let valorUsuario = document.getElementById("valor-usuario");
+valorUsuario.addEventListener("keypress", function(event) {
+
+    console.log(event);
+
+    if(event.ctrlkey == true && event.code == "KeyI"){
+        inverter();
+    }
+
+    if(event.ctrlkey == true && event.code == "KeyL"){
+        limpar(); //verificar pq está dando erro
+    }
+
+    if(event.key == "Enter"){
+        converter();
+    }
+
+});
+
+function salvaResultadoHistorico(conversao) {
+    let historico = recuperarHistoricodeConversoes();
+
+    historico.push(conversao);
+
+    historico = JSON.stringify(historico);
+    localStorage.setItem("historico", historico);
+}
+
+function recuperarHistoricodeConversoes(){
+    let historico = localStorage.getItem("historico");
+
+    if(!historico){
+        return[];
+    }
+    let historicoConvertido = JSON.parse(historico);
+    return historicoConvertido;
+}
+
+
+
+function aceitaMensagem(){
+    let divMensagemUsuario = document.getElementById("container-mensagem-usuario");
+    divMensagemUsuario.classList.add("oculto");
+
+    localStorage.setItem("aceitouCookie", "1")
+}
+
+
+function limpar(){
+    let valorUsuario = document.getElementById("valor-usuario");
+    let resultado = document.getElementById("resultado");
+
+    valorUsuario.value = "";
+    resultado.textContent = "";
+}
+
+
+function converter() {
+    buscaAPI();
+    let valorUsuario = document.getElementById("valor-usuario").value;
+
+    let moedaOrigem = document.getElementById("moeda1").value;
+    let moedaDestino = document.getElementById("moeda2").value;
+
+    if(valorUsuario == ""){
+        alert("Valor não pode ser vazio!")
+        return;
+    }
+
+    if(valorUsuario < 0) {
+        alert("Valor usuário não pode ser negativo");
+        return;
+    }
+
+
+    if(moedaOrigem == moedaDestino){
+        alert("As moedas são iguais, não é possível converter");
+        return;
+        
+    }
+
+    let conversao = valorUsuario * valoresConversao[moedaOrigem][moedaDestino];
+
+    let simbolo = "";
+    if (moedaDestino == "real"){
+        simbolo = "R$"
+    }
+    if (moedaDestino == "dolar"){
+        simbolo = "US$"
+    }
+    if (moedaDestino == "euro"){
+        simbolo = "€";
+    }
+
+
+let paragrafoResultado = document.getElementById("resultado");
+paragrafoResultado.textContent = simbolo + " " + conversao.toFixed(2);
+
+let resultadoDaConversao = {
+    valor: valorUsuario,
+    moeda1: moeda1,
+    moeda2: moedaDestino,
+    resultado: conversao
+}
+
+}
+function inverter(){
+    let moeda1 = document.getElementById("moeda1").value;
+    let moeda2 = document.getElementById("moeda2").value;
+
+    document.getElementById("moeda1").value = moeda2;
+    document.getElementById("moeda2").value = moeda1;
+
+    console.log(moeda1);
+    console.log(moeda2);
+}
