5d81cc5cdd7b334cc7021c1a69c26da01583a627
Camila <162577864+camis08@users.noreply.github.com>
2024-05-02 10:50:05 -0300

    Add files via upload
    02/05 sem mudanças do prof

--- conversorMoedas.js
+++ conversorMoedas.js
@@ -1,26 +1,9 @@
-let valoresConversao = {
-    real: {
-        dolar: 0.27,
-        euro: 0.18
-    },
-    dolar: {
-        real: 5.03,
-        euro: 0.92,
-    },
-    euro: {
-        real: 5.47,
-        dolar: 0.92
-    }
-}
 
-
-
-let botaoConverter= document.getElementById("botao-converter");
+let botaoConverter = document.getElementById("botao-converter");
 botaoConverter.addEventListener("click", converter);
 
-let botaoLimpar= document.getElementById("botao-limpar");
+let botaoLimpar = document.getElementById("botao-limpar");
 botaoLimpar.addEventListener("click", limpar);
-
 
 let botaoInverter = document.getElementById("botao-inverter");
 botaoInverter.addEventListener("click", inverter);
@@ -28,34 +11,12 @@ botaoInverter.addEventListener("click", inverter);
 let botaoAceitaMensagem = document.getElementById("botao-aceita-mensagem");
 botaoAceitaMensagem.addEventListener("click", aceitaMensagem);
 
-if(localStorage.getItem("aceitouCookie" == "1")){
+if(localStorage.getItem("aceitouCookie") == "1") {
     aceitaMensagem();
-
 }
 
-
-
-let valorUsuario = document.getElementById("valor-usuario");
-valorUsuario.addEventListener("keypress", function(event) {
-
-    console.log(event);
-
-    if(event.ctrlkey == true && event.code == "KeyI"){
-        inverter();
-    }
-
-    if(event.ctrlkey == true && event.code == "KeyL"){
-        limpar(); //verificar pq está dando erro
-    }
-
-    if(event.key == "Enter"){
-        converter();
-    }
-
-});
-
-function salvaResultadoHistorico(conversao) {
-    let historico = recuperarHistoricodeConversoes();
+function salvaResultadoNoHistorico(conversao) {
+    let historico = recuperaHistoricoDeConversoes();
 
     historico.push(conversao);
 
@@ -63,27 +24,45 @@ function salvaResultadoHistorico(conversao) {
     localStorage.setItem("historico", historico);
 }
 
-function recuperarHistoricodeConversoes(){
+function recuperaHistoricoDeConversoes() {
     let historico = localStorage.getItem("historico");
 
-    if(!historico){
-        return[];
+    if(!historico) {
+        return [];
     }
     let historicoConvertido = JSON.parse(historico);
     return historicoConvertido;
 }
 
-
-
-function aceitaMensagem(){
+function aceitaMensagem() {
     let divMensagemUsuario = document.getElementById("container-mensagem-usuario");
     divMensagemUsuario.classList.add("oculto");
 
-    localStorage.setItem("aceitouCookie", "1")
+    localStorage.setItem("aceitouCookie", "1");
 }
 
 
-function limpar(){
+let valorUsuario = document.getElementById("valor-usuario");
+valorUsuario.addEventListener("keypress", function(event) {
+
+    //console.log(event);
+
+    if(event.ctrlKey == true && event.code == "KeyI") {
+        inverter();
+    }
+
+    if(event.ctrlKey == true && event.code == "KeyL") {
+        limpar();
+    }
+
+    if(event.key == "Enter") {
+        converter();
+    }
+
+});
+
+
+function limpar() {
     let valorUsuario = document.getElementById("valor-usuario");
     let resultado = document.getElementById("resultado");
 
@@ -91,58 +70,111 @@ function limpar(){
     resultado.textContent = "";
 }
 
+function buscaAPI(moedaOrigem="USD", moedaDestino="BRL") {
+    
+    console.log(moedaOrigem);
+    console.log(moedaDestino);
 
+    let parametro = moedaOrigem + "-" + moedaDestino;
+    let url = "https://economia.awesomeapi.com.br/json/last/" + parametro;
+
+    console.log(url);
+
+    return fetch(url).then(function(data){
+        if(data.status == 200) {
+            console.log("Retorno código 200 API!");
+        }
+        return data.json();
+    }).then(function(response){
+        return response[moedaOrigem + moedaDestino];
+
+        console.log(objetoEmJson);
+
+        //console.log(response["USDBRL"]["ask"]);
+        //console.log(typeof(response["USDBRL"]["ask"]));
+        
+        return response;
+        //return response;
+        //return response["USDBRL"]["ask"];
+    }).catch();
+}
 
 
 function converter() {
     let valorUsuario = document.getElementById("valor-usuario").value;
 
-    let moedaOrigem = document.getElementById("moeda1").value;
+    let moedaOrigem  = document.getElementById("moeda1").value;
     let moedaDestino = document.getElementById("moeda2").value;
 
-    if(valorUsuario == ""){
-        alert("Valor não pode ser vazio!")
+    /*
+    if (moedaOrigem == "real") {
+        urlAPIParametroMoedaOrigem = "BRL";
+    }
+    if (moedaOrigem == "euro") {
+        urlAPIParametroMoedaOrigem = "EUR";
+    }
+    if (moedaOrigem == "dolar") {
+        urlAPIParametroMoedaOrigem = "USD";
+    }
+    */
+    
+    if(valorUsuario == "") {
+        alert("Valor não pode ser vazio!");
         return;
     }
 
-    if(moedaOrigem == moedaDestino){
+    if(valorUsuario < 0) {
+        alert("Valor não pode ser negativo");
+        return;
+    }
+
+    if(moedaOrigem == moedaDestino) {
         alert("As moedas são iguais, não é possível converter");
         return;
+    }
+
+    buscaAPI(moedaOrigem, moedaDestino).then(function(data){
+        //let objetoRetorno = JSON.parse(data);
+        let conversao = valorUsuario * data["ask"];
+
+
+        let simbolo = "";
+        if (moedaDestino == "BRL") {
+            simbolo = "R$";
+        }
+        if (moedaDestino == "USD") {
+            simbolo = "US$"
+        }
+        if (moedaDestino == "EUR") {
+            simbolo = "€";
+        }
+        if (moedaDestino == "GBP") {
+            simbolo = "£";
+        }
         
-    }
+        let resultado = document.getElementById("resultado");
+        resultado.textContent = simbolo + " " + conversao.toFixed(2);
 
-    let conversao = valorUsuario * valoresConversao[moedaOrigem][moedaDestino];
+        let resultadoDaConversao = {
+            valor: valorUsuario,
+            moeda1: moedaOrigem,
+            moeda2: moedaDestino,
+            resultado: conversao
+        }
 
-    let simbolo = "";
-    if (moedaDestino == "real"){
-        simbolo = "R$"
-    }
-    if (moedaDestino == "dolar"){
-        simbolo = "US$"
-    }
-    if (moedaDestino == "euro"){
-        simbolo = "€";
-    }
+        salvaResultadoNoHistorico(resultadoDaConversao);
 
+        console.log(conversao);
 
-let paragrafoResultado = document.getElementById("resultado");
-paragrafoResultado.textContent = simbolo + " " + conversao.toFixed(2);
+        console.log(data["ask"]);
+    });
+    return;
 
-let resultadoDaConversao = {
-    valor: valorUsuario,
-    moeda1: moeda1,
-    moeda2: moedaDestino,
-    resultado: conversao
-}
-
-}
-function inverter(){
+function inverter() {
     let moeda1 = document.getElementById("moeda1").value;
     let moeda2 = document.getElementById("moeda2").value;
 
+
     document.getElementById("moeda1").value = moeda2;
     document.getElementById("moeda2").value = moeda1;
-
-    console.log(moeda1);
-    console.log(moeda2);
 }
